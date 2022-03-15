32a692a7becca7807302d3f805b3299a035bbc8d
Ana Matias <ana17mat@gmail.com>
2022-03-15 16:53:32 +0000

    ok

--- finger_pose/test5.py
+++ finger_pose/test5.py
@@ -1,9 +1,11 @@
 from cv2 import imread
-import cvzone
+#import cvzone
 import cv2
 import numpy as np
 from cvzone.HandTrackingModule import HandDetector
 import random
+
+import time
 
 from scipy.spatial import distance
 import pickle
@@ -27,10 +29,13 @@ detector = HandDetector(detectionCon=0.8, maxHands=1)
 
 
 preds = [" "]
+preds_str = "".join(preds).strip()
+preds_m = [" ", " ", " ", " ", " "]
 
 # dict_asl = pickle.load(open("dictletras_a_to_t.pkl", "rb"))
 
-knn_asl = pickle.load(open("knn_asl.p", "rb"))
+knn_asl = pickle.load(
+    open("/Users/anamatias/Desktop/FINAL_PROJECT/asl_project/knn_asl.p", "rb"))
 
 
 positions = []
@@ -41,20 +46,27 @@ while True:
     # hands, img = detector.findHands(img, flipType=False)
     hands, img = detector.findHands(img, flipType=False)
 
+    cv2.putText(
+        img,
+        preds_str,
+        # f"LETRA:{str('A')}",
+        (20, 650),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        1,
+        (255, 0, 0),
+        2,
+    )
+
     if hands:
         lmList = hands[0]["lmList"]
         pos = [l[0:2] for l in lmList]
         originwrist = [[pos[0][0] - l[0], pos[0][1] - l[1]] for l in pos]
         positions.append(pos)
-        cv2.putText(
-            img,
-            f"LETRA:{str('A')}",
-            (20, 70),
-            cv2.FONT_HERSHEY_SIMPLEX,
-            1,
-            (0, 255, 0),
-            2,
-        )
+        hpred = [item for sublist in originwrist for item in sublist]
+        letramin = knn_asl.predict([hpred])[0]
+        preds_m.append(letramin)
+        preds_m = preds_m
+        print(preds_m)
 
     cv2.imshow("ASL", img)
 
@@ -62,6 +74,7 @@ while True:
 
     if not hands and preds[-1] != " ":
         preds.append(" ")
+        preds_str = "".join(preds).strip()
         print(" ")
 
     # pred with space key
@@ -69,17 +82,22 @@ while True:
         hpred = [item for sublist in originwrist for item in sublist]
         letramin = knn_asl.predict([hpred])[0]
         preds.append(letramin)
+        preds_str = "".join(preds).strip()
+        print(letramin)
 
     # pred to string to speech
-    if key == ord("s"):
-        preds_str = "".join(preds).strip()
-        print(preds_str)
-        print("im here")
-        engine.say(preds_str)
-        engine.runAndWait()
-        print("now here", key)
-        preds = [" "]
+    # if key == ord("s"):
+    #     preds_str = "".join(preds).strip()
+    #     print(preds_str)
+    #     print("im here")
+    #     engine.say(preds_str)
+    #     tnow = time.time()
+    #     while time.time()-tnow < 10:
+    #         engine.runAndWait()
+    #     print("now here", key)
+    #     preds = [" "]
 
     # CLOSE WITH ESC KEY
     if key == 27:
+        print(preds)
         break
